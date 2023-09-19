31f12fb0aa605cd425fc1c66517852c29c732c84
River <riverdejesus2@gmail.com>
2023-09-19 10:40:23 -0400

    Book id, title, genreId

--- demo/src/main/java/com/example/demo/model/Book.java
+++ demo/src/main/java/com/example/demo/model/Book.java
@@ -1,4 +1,13 @@
 package com.example.demo.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    private Long id;
+    private String title;
+    private Long genreId;
 }
