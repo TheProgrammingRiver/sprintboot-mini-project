44572df176c1ec1ae6092801c04f963cd68aed38
River <riverdejesus2@gmail.com>
2023-09-19 10:31:16 -0400

    id, Username, Password

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,13 @@
 package com.example.demo.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    private Long id;
+    private String Username;
+    private String Password;
 }
