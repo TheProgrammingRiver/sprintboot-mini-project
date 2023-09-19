d2ac31d869d3ea64af46ed7747ce9a7d5b89c8fa
River <riverdejesus2@gmail.com>
2023-09-19 11:06:21 -0400

    Add a new genre to the database.

--- demo/src/main/java/com/example/demo/service/GenreService.java
+++ demo/src/main/java/com/example/demo/service/GenreService.java
@@ -1,4 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Genre;
+import com.example.demo.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GenreService {
+    @Autowired
+    private GenreRepository repo;
+    
+    public Genre addNewGenre(Genre newGenre) {
+        return repo.save(newGenre);
+    }
 }
