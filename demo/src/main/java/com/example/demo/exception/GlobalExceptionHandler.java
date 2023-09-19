c4599a1b7870cd1a85fd0ec2e62b56823686c967
River <riverdejesus2@gmail.com>
2023-09-19 18:15:39 -0400

    Got a custom exception

--- demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ demo/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.example.demo.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = CustomException.class)
+    @ResponseBody
+    public ResponseEntity<Object> handleCustomException(CustomException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    
 }
+
