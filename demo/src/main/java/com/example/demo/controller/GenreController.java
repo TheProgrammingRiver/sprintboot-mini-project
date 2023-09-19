d45fa366337389de937442f56bfed386d5a6a340
River <riverdejesus2@gmail.com>
2023-09-19 14:48:16 -0400

    Get rid of a genre.

--- demo/src/main/java/com/example/demo/controller/GenreController.java
+++ demo/src/main/java/com/example/demo/controller/GenreController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/genres")
 public class GenreController {
     @Autowired
-    private GenreService genreService;
+    private GenreService genreSrv;
+
+    @DeleteMapping("/{id}")
+    public void removeGenre(@PathVariable Long id) {
+        genreSrv.removeGenre(id);
+    }
 }
