9e6446a3ce47fcc55fab8272944975cb2dbcf5e3
River <riverdejesus2@gmail.com>
2023-09-19 10:35:52 -0400

    Genre- id, name

--- demo/src/main/java/com/example/demo/model/Genre.java
+++ demo/src/main/java/com/example/demo/model/Genre.java
@@ -1,4 +1,12 @@
 package com.example.demo.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Genre {
+    @Id
+    private Long id;
+    private String name;
 }
