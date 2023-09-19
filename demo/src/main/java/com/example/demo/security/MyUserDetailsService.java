180ce271fea947cfb74c237ce14bdd602dfb6ca2
River <riverdejesus2@gmail.com>
2023-09-19 18:05:53 -0400

    Get the user details. Throws an exception if the user is missing.

--- demo/src/main/java/com/example/demo/security/MyUserDetailsService.java
+++ demo/src/main/java/com/example/demo/security/MyUserDetailsService.java
@@ -1,4 +1,33 @@
 package com.example.demo.security;
 
-public class MyUserDetailsService {
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User foundUser = userRepo.findByUsername(username);
+        if (foundUser == null) {
+            throw new UsernameNotFoundException("user doesn't exist.");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                foundUser.getUsername(), foundUser.getPassword(), new ArrayList<>());
+    }
 }
+
+
+
+
+
