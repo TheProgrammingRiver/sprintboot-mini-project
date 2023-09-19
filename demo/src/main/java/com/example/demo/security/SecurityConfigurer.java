1b0a8b4d1491f741a7322ac8f88f5485c8eb11ae
River <riverdejesus2@gmail.com>
2023-09-19 17:54:06 -0400

    configure

--- demo/src/main/java/com/example/demo/security/SecurityConfigurer.java
+++ demo/src/main/java/com/example/demo/security/SecurityConfigurer.java
@@ -1,4 +1,20 @@
 package com.example.demo.security;
 
-public class SecurityConfigurer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private MyUserDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/api/login").permitAll()
+                .anyRequest().authenticated();
+    }
 }
