be826d50997fbc29a05abf706cd739e7f862c925
River <riverdejesus2@gmail.com>
2023-09-19 13:31:32 -0400

    Update GenreController

--- demo/src/main/java/com/example/demo/controller/GenreController.java
+++ demo/src/main/java/com/example/demo/controller/GenreController.java
@@ -1,4 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/genres")
 public class GenreController {
+    @Autowired
+    private GenreService genreService;
 }
