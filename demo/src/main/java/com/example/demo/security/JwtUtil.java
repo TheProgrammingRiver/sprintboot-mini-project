b6a4f1df1d777520cfe759285c7c0d49e1a995f7
River <riverdejesus2@gmail.com>
2023-09-19 17:42:11 -0400

    Need a token

--- demo/src/main/java/com/example/demo/security/JwtUtil.java
+++ demo/src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,10 +1,18 @@
 package com.example.demo.security;
 
-
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtUtil {
 
     private String SECRET_KEY = "secret";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
 }
