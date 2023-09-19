28a47cd2f22927e493ffa0b6635336fcc8f8a7bb
River <riverdejesus2@gmail.com>
2023-09-19 13:39:50 -0400

    Delete a user by ID.

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
     @Autowired
-    private UserService userService;
+    private UserService userSrv;
+
+    @DeleteMapping("/{id}")
+    public void removeUser(@PathVariable Long id) {
+        userSrv.removeUser(id);
+    }
 }
