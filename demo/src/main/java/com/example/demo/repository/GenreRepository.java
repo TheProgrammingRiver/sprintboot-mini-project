c57c0ac7a5bb6583ceb6726910277b1dab9c1de8
River <riverdejesus2@gmail.com>
2023-09-19 10:48:08 -0400

    GenreRepository interface

--- demo/src/main/java/com/example/demo/repository/GenreRepository.java
+++ demo/src/main/java/com/example/demo/repository/GenreRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repository;
 
-public class GenreRepository {
+import com.example.demo.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
