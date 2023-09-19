125b68bfb7a1cb1b66436c5f6bbaac88944fc92b
River <riverdejesus2@gmail.com>
2023-09-19 11:42:08 -0400

    see all the books.

--- demo/src/main/java/com/example/demo/service/BookService.java
+++ demo/src/main/java/com/example/demo/service/BookService.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookService {
     @Autowired
@@ -21,12 +23,16 @@ public class BookService {
     public Book fetchBook(Long id) {
         return bookRepo.findById(id).orElse(null);
     }
-    
+
     public Book updateBookInfo(Long id, Book updatedBook) {
         if (bookRepo.existsById(id)) {
             updatedBook.setId(id);
             return bookRepo.save(updatedBook);
         }
         return null; // No book found.
+    }
+
+    public List<Book> fetchAllBooks() {
+        return bookRepo.findAll();
     }
 }
