579ec4abde965f9fb707d2d584df5bf2328ba952
River <riverdejesus2@gmail.com>
2023-09-19 10:34:11 -0400

    User-Getters and setters

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -10,4 +10,28 @@ public class User {
     private Long id;
     private String Username;
     private String Password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
