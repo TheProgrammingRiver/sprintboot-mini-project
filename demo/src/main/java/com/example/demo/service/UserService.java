a1485efb2c3c45088c4f665bf0efc3871e3e755e
River <riverdejesus2@gmail.com>
2023-09-19 22:25:17 -0400

    Update UserService.java

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.CustomException;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,31 +12,30 @@ import java.util.List;
 
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepo;
+        @Autowired
+        private UserRepository userRepo;
 
-    public User addNewUser(User newUser) {
-        return userRepo.save(newUser);
-    }
-
-    public void removeUser(Long userId) {
-        userRepo.deleteById(userId);
-    }
-
-    public User fetchUser(Long id) {
-        return userRepo.findById(id).orElse(null);
-    }
-
-    public User updateUser(Long id, User updatedUser) {
-        if (userRepo.existsById(id)) {
-            updatedUser.setId(id);
-            return userRepo.save(updatedUser);
+        public User addNewUser(User newUser){
+            return userRepo.save(newUser);
         }
-        return null; // Couldn't find the user
-    }
 
-    public List<User> getAllUsers() {
-        return userRepo.findAll();
-    }
+        public void removeUser (Long userId){
+            userRepo.deleteById(userId);
+        }
 
-}
+        public User fetchUser (Long id){
+            return userRepo.findById(id).orElse(null);
+        }
+
+        public User updateUser (Long id, User updatedUser){
+            if (userRepo.existsById(id)) {
+                updatedUser.setId(id);
+                return userRepo.save(updatedUser);
+            }
+            return null; // Couldn't find the user
+        }
+
+        public List<User> getAllUsers () {
+            return userRepo.findAll();
+        }
+    }
