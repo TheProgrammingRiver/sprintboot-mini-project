c7e6089a61954291567d0305b4a318a3b2fc63c3
River <riverdejesus2@gmail.com>
2023-09-19 22:24:48 -0400

    Create GenreDetailsService.java

---
+++ demo/src/main/java/com/example/demo/security/GenreDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.demo.security;
+
+import com.example.demo.model.Genre;
+import com.example.demo.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class GenreDetailsService {
+
+    @Autowired
+    private GenreRepository genreRepo;
+
+    public Genre loadGenreByName(String name) {
+        Genre foundGenre = genreRepo.findByName(name);
+        if (foundGenre == null) {
+            throw new RuntimeException("Genre doesn't exist.");
+        }
+        return new Genre((Long) foundGenre.getId(), foundGenre.getName(), foundGenre.getGenre());
+    }
+}
