3ad368c86d2d1cca063fd4663f8b1c3a80550646
River <riverdejesus2@gmail.com>
2023-09-19 23:17:27 -0400

    Update MyUserDetailsService.java

--- demo/src/main/java/com/example/demo/security/MyUserDetailsService.java
+++ demo/src/main/java/com/example/demo/security/MyUserDetailsService.java
@@ -15,7 +15,13 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepo;
-
+    /**
+     * Loads a user object from the database based on the given username.
+     *
+     * @param  username  the username of the user to load
+     * @return           the UserDetails object representing the loaded user
+     * @throws UsernameNotFoundException if the user doesn't exist in the database
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User foundUser = userRepo.findByUsername(username);
